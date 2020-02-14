bbe06cd6f7523d3e5d89e8599ebc544ee06df87d
Sunilv390 <sunilv390@gmail.com>
2020-02-14 11:37:57 +0530

    Modified: Added function to check number is palindrome

--- function/PrimePalindrome.sh
+++ function/PrimePalindrome.sh
@@ -2,6 +2,24 @@
 
 read -p "Enter number : " numbers
 
+#FUNCTION TO CHECK IF NUMBER IS PALINDROME
+function isPalindrome(){
+sum=0;
+number=$1
+while [ $number -gt 0 ]
+do
+	remainder=$((number%10));
+	sum=$((sum*10+remainder));
+	number=$((number/10));
+done
+if [ $sum -eq $1 ]
+then
+	echo " $numbers is Palindrome"
+else
+	echo "$numbers is not Palindrome "
+fi
+}
+
 #FUNCTION TO CHECK IF NUMBER IS PRIME
 function isPrime(){
 isPrime=0;
@@ -21,5 +39,5 @@ else
 fi
 }
 echo "$( isPrime $numbers )"
-
+echo "$( isPalindrome $numbers )"
 
