9adeae8d814b9c7dcf409e5969c60b0bb546c617
Sunilv390 <sunilv390@gmail.com>
2020-02-14 10:50:26 +0530

    Added: Prints prime number between 2 values

---
+++ forloop/PrimeRange.sh
@@ -0,0 +1,25 @@
+#!/bin/bash  -x
+
+#READ UPPER AND LOWER LIMIT
+read -p "Enter a number" start
+read -p "Enter a number" end
+
+#CHECKS PRIME RANGE
+for (( i=start; i<=end; i++ ))
+do
+   count=0
+   for (( j=1; j<=i; j++ ))
+   do
+      if [ $((i%j)) -eq 0 ]
+      then
+         count=$((count+1))
+      fi
+   done
+
+#PRINTS RANGE
+if [ $count -eq 2 ]
+then
+   echo "$i is the Prime number between $start and $end "
+fi
+done
+
